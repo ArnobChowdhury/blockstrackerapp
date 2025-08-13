c73d1d554d1774c35fcc2e471068c0f37f60697f
ArnobChowdhury <chowdhury.sami.al@gmail.com>
2025-08-13 16:42:24 +0600

    fix(google sign in): Fix google sign in

--- src/services/googleSignIn.ts
+++ src/services/googleSignIn.ts
@@ -3,7 +3,7 @@ import { GoogleSignin } from '@react-native-google-signin/google-signin';
 export const configureGoogleSignIn = () => {
   GoogleSignin.configure({
     webClientId:
-      '513711750326-cim84g85urtac13qhu2mrt8sg6c10tkl.apps.googleusercontent.com',
+      '513711750326-uppblult7i4m91tq66at4g30tfcin4ck.apps.googleusercontent.com',
     offlineAccess: true,
   });
 };
